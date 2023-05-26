4423d30babcb4b7729fc07cc708feb072978a3b3
jorgemagic <jferrero@plainconcepts.com>
2023-05-26 10:56:12 +0200

    - Included Maui profile

---
+++ EvergineIntegration.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EvergineIntegration.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EvergineIntegration.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EvergineIntegration.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
