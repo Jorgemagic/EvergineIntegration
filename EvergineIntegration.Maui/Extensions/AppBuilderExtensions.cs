6ea762b86cd25db2956f360a126d1242fc2d1729
jorgemagic <jferrero@plainconcepts.com>
2023-06-06 15:29:12 +0200

    - Added some files

---
+++ EvergineIntegration.Maui/Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using EvergineIntegration.Maui.Controls;
+using EvergineIntegration.Maui.Handlers;
+
+namespace EvergineIntegration.Maui.Extensions
+{
+    public static class AppBuilderExtensions
+    {
+        public static MauiAppBuilder UseMauiEvergine(this MauiAppBuilder builder)
+        {
+            builder.ConfigureMauiHandlers(h =>
+            {
+                h.AddHandler<EvergineView, EvergineViewHandler>();
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ EvergineIntegration.Maui/Handlers/EvergineViewHandler.cs
@@ -0,0 +1,29 @@
+﻿#if IOS || MACCATALYST
+using PlatformView = System.Object;
+#elif ANDROID
+using PlatformView = System.Object;
+#elif WINDOWS
+using PlatformView = Microsoft.UI.Xaml.Controls.SwapChainPanel;
+#elif NETSTANDARD || !PLATFORM || (NET6_0_OR_GREATER && !IOS && !ANDROID)
+using PlatformView = System.Object;
+#endif
+
+using Microsoft.Maui.Handlers;
+using EvergineIntegration.Maui.Controls;
+
+namespace EvergineIntegration.Maui.Handlers
+{
+    public partial class EvergineViewHandler
+    {
+        public static IPropertyMapper<EvergineView, EvergineViewHandler> PropertyMapper = new PropertyMapper<EvergineView, EvergineViewHandler>(ViewMapper)
+        {
+            [nameof(EvergineView.Application)] = MapApplication,
+        };
+
+        public static CommandMapper<EvergineView, EvergineViewHandler> CommandMapper = new(ViewCommandMapper);
+
+        public EvergineViewHandler() : base(PropertyMapper, CommandMapper)
+        {
+        }
+    }
+}
